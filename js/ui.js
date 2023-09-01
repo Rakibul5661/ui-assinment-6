c8a7fd42c8f6d4656aa34949e1405624bf2be94e
User <rafidulhaque56@gmail.com>
2023-09-01 15:07:54 +0600

    added fetures and intrintegration

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 const loadData = () =>{
-    fetch(' https://openapi.programming-hero.com/api/ai/tools')
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
     .then(data => displayAi(data.data.tools))
 }
@@ -9,13 +9,13 @@ const loadData = () =>{
 const displayAi = (allAi) =>{
     const cardDiv = document.getElementById('ai-card');
     allAi.forEach(ai => {
-        // console.log(ai)
+        // console.log(ai.id)
         const card = document.createElement('div');
         card.classList.add('col')
         card.innerHTML = `
             <div class="card">
                 <div style="height:200px;" class="p-4">
-                    <img src="${ai.image}" class="card-img-top rounded-3 img-fluid " alt="...">
+                    <img src="${ai.image}" class="card-img-top rounded-3 img-fluid " alt="..."> 
                 </div>
                 <div class="card-body">
                     <div class="border-bottom mb-4">
@@ -32,7 +32,7 @@ const displayAi = (allAi) =>{
                             <p>📅 ${ai.published_in}</p>
                         </div>
                         <div>
-                            <button onclick="loadDetails(${ai.id})" class="rounded-circle" data-bs-toggle="modal" data-bs-target="#show-details">→</button>
+                            <button onclick="loadDetails('${ai.id}')" class="rounded-circle" data-bs-toggle="modal" data-bs-target="#show-details">→</button>
                         </div>
                     </div>
                 </div>
@@ -44,39 +44,35 @@ const displayAi = (allAi) =>{
 
 // display modal
  const loadDetails = (Id) =>{
-     const url = `https://openapi.programming-hero.com/api/ai/tool/0${Id}`
+     const url = `https://openapi.programming-hero.com/api/ai/tool/${Id}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayDetails(data.data))
  }
 
  const displayDetails = (detail) =>{
-    console.log(detail)
+    console.log(detail.pricing)
     const detailsDiv = document.getElementById('details');
     detailsDiv.innerHTML = `
     <div style="background-color:#fef7f7; width:48%;" class="border border-danger-subtle p-4 rounded-4">
         <div class="">
             <h6 class="fw-bold">${detail.description}</h6>
-            <div class="d-flex gap-2 justify-content-between">
-                <div style="width:30%; background-color:white;" class="text-center rounded-4 fw-bold"><p>${detail.pricing[0].price} <br> ${detail.pricing[0].plan}</p></div>
-                <div style="width:30%; background-color:white;" class="text-center rounded-4 fw-bold"><p>${detail.pricing[1].price} <br> ${detail.pricing[1].plan}</p></div>
-                <div style="width:30%; background-color:white;" class="text-center rounded-4 fw-bold"><p>${detail.pricing[2] ? detail.pricing[2].price : 'no plan' } <br> ${detail.pricing[2].plan}</p></div>
+            <div class="d-flex gap-2 justify-content-between my-4">
+                <div style="width:30%; background-color:white; font-size:14px;" class="text-center text-success rounded-4 fw-bold d-flex align-items-center p-2 justify-content-center"><p></p></div>
+                <div style="width:30%; background-color:white; font-size:14px;" class="text-center text-warning rounded-4 fw-bold d-flex align-items-center p-2 justify-content-center"><p></p></div>
+                <div style="width:30%; background-color:white; font-size:14px;" class="text-center text-danger rounded-4 fw-bold d-flex align-items-center p-2 justify-content-center"><p></p></div>
             </div>
             <div class="d-flex justify-content-between g-2">
                 <div class="w-50">
                     <h6 class="fw-bold">Features</h6>
-                    <ul>
-                        <li>${detail.features[1].feature_name}</li>
-                        <li>${detail.features[2].feature_name}</li>
-                        <li>${detail.features[3].feature_name}</li>
+                    <ul id="features-list">
+
                     </ul>
                 </div>
-                <div>
+                <div style="width:46%;">
                     <h6 class="fw-bold">Integrations</h6>
-                    <ul>
-                        <li>yy</li>
-                        <li>yy</li>
-                        <li>yy</li>
+                    <ul id="integrations-list">
+                        
                     </ul>
                 </div>
             </div>
@@ -91,6 +87,25 @@ const displayAi = (allAi) =>{
         </div>
     </div>
     `;
+    
+
+    const featuresList = document.getElementById('features-list');
+    const featuresData = detail.features;
+    for(const featuresKey in featuresData){
+        const feature = featuresData[featuresKey].feature_name;
+        const listItem = document.createElement('li');
+        listItem.innerText = feature;
+        featuresList.appendChild(listItem) 
+    }
+
+    const integrationsList = document.getElementById("integrations-list");
+    const integrationsdata = detail.integrations;
+    for(const integration of integrationsdata){
+            const listItem = document.createElement("li");
+            listItem.innerText = integration;
+            integrationsList.appendChild(listItem);
+    }
+
  }
 
 
